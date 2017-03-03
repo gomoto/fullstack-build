a0c526a4f74c11214452c1c738c6c17b75e1510a
Ryan Gomoto <ryan@cyrusbio.com>
2017-03-03 14:42:36 -0800

    Require config from project

--- gulpfile.js
+++ gulpfile.js
@@ -120,66 +120,9 @@ function removeEmptyDirectory(current, root) {
   removeEmptyDirectory(path.dirname(current), root);
 }
 
-
-const names = {
-  app: 'app',
-  client: 'client',
-  server: 'server'
-};
-
-const config = {
-  client: {
-    html: {
-      entry: `${names.client}/src/index.html`,
-      bundle: `${names.app}/${names.client}/index.html`,
-      watch: `${names.client}/src/**/*.html`,
-      inject: {
-        templates: {
-          globs: [`${names.client}/src/*/**/*.html`]
-        },
-        css: {
-          globs: [`${names.app}/${names.client}/index-*.css`],
-          cwd: `${names.app}/${names.client}`
-        },
-        js: {
-          globs: [
-            `${names.app}/${names.client}/vendor-*.js`,
-            `${names.app}/${names.client}/index-*.js`
-          ],
-          cwd: `${names.app}/${names.client}`
-        }
-      }
-    },
-    scss: {
-      entry: `${names.client}/src/index.scss`,
-      bundle: `${names.app}/${names.client}/index.css`,
-      watch: `${names.client}/src/**/*.scss`
-    },
-    ts: {
-      entry: `${names.client}/src/index.ts`,
-      bundle: `${names.app}/${names.client}/index.js`,
-      watch: `${names.client}/src/**/*.ts`,
-      tsconfig: `${names.client}/tsconfig.json`
-    },
-    vendors: {
-      manifest: `${names.client}/vendors.json`,
-      bundle: `${names.app}/${names.client}/vendor.js`
-    }
-  },
-  server: {
-    from: `${names.server}/src`,
-    to: `${names.app}/${names.server}`,
-    tsconfig: `${names.server}/tsconfig.json`
-  },
-  resources: {
-    images: {
-      from: `${names.client}/assets/images`,
-      to: `${names.app}/${names.client}/assets/images`,
-      manifest: `${names.app}/${names.client}/assets/images/manifest.json`
-    }
-  },
-  gitCommit: `${names.app}/git-sha.txt`
-};
+// Required.
+// TODO: Make optional.
+const config = require(path.join(process.cwd(), 'fullstack.conf.js'))();
 
 const paths = {
   env: '.env'
